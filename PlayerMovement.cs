1f7a2087be57404419f9525821eebb6026c0e55f
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-08-05 20:09:49 -0400

    Updated PlayerMovement.cs
    Rewrote the original game code with a lot of refactoring to use traditional flight and speed movements and now works correctly.

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,52 +4,88 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 0f;
-    public float jumpForce;
-    public float gravity = 20f;
-    private float verticalVelocity;
-    private bool isGrounded;
-    private CharacterController controller;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float flySpeed = 10f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float flyForce = 15f;
+    [SerializeField] private float rotationSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private Rigidbody rb;
+    private bool isGrounded;
+    private bool isFlying;
+    private bool isAttacking;
+
+    private void Awake()
     {
-        isGrounded = controller.isGrounded;
-        PlayerMovementHandler();
-        ApplyGravity();
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Check if the player is grounded
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
+
+        // Handle movement using traditional Unity Input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 moveInput = new Vector2(horizontalInput, verticalInput);
+        Move(moveInput);
+
+        // Handle jumping and flying using traditional Unity Input
+        if (Input.GetButtonDown("Jump"))
         {
-            JumpInput();
+            if (isGrounded)
+                Jump();
+            else
+                ToggleFlying();
         }
     }
-    private void PlayerMovementHandler()
+
+    private void Move(Vector2 moveInput)
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticialInput = Input.GetAxis("Vertical");
-        Vector3 moveDirection = (transform.forward * verticialInput + transform.right * horizontalInput).normalized;
-        moveDirection *= moveSpeed;
-        controller.Move(moveDirection * Time.deltaTime);
-    }
-    public void ApplyGravity()
-    {
-        if (!isGrounded)
+        // Apply horizontal movement
+        Vector3 moveDirection = transform.forward * moveInput.y * moveSpeed;
+        rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+
+        // Handle rotation
+        if (moveInput.x != 0f)
         {
-            verticalVelocity -= gravity * Time.deltaTime;
+            float rotationY = transform.eulerAngles.y + (moveInput.x * rotationSpeed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0f, rotationY, 0f);
+        }
+
+        // Apply vertical movement (fly up/down)
+        if (isFlying)
+        {
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 flyDirection = transform.up * verticalInput * flySpeed;
+            rb.velocity = new Vector3(rb.velocity.x, flyDirection.y, rb.velocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+    }
+
+    private void ToggleFlying()
+    {
+        if (isFlying)
+        {
+            rb.useGravity = true;
+            isFlying = false;
         }
         else
         {
-            verticalVelocity = -0.1f;
+            rb.useGravity = false;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector3.up * flyForce, ForceMode.Impulse);
+            isFlying = true;
         }
-        Vector3 gravityVector = new Vector3(0, verticalVelocity, 0);
-        controller.Move(gravityVector * Time.deltaTime);
-    }
-    public void JumpInput()
-    {
-        verticalVelocity = jumpForce;
     }
 }
