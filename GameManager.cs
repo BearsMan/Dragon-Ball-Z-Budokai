44e22489c57da8f147442f362beccbec2e624dfd
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-08-08 11:05:03 -0400

    Updated GameManager.cs
    changed the variable name in the code in the static line.

--- GameManager.cs
+++ GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    public static GameManager currentGame;
     // Start is called before the first frame update
     void Start()
     {
